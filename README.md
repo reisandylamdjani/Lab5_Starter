9527accb96ab183673a6bdeb78639523117b5bf9
reisandylamdjani <lamdjani@gmail.com>
2024-05-04 21:09:37 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Reisandy Lamdjani
 Nicholas Quach
 
 [Expose](https://reisandylamdjani.github.io/Lab5_Starter/expose.html)
+
 [Explore](https://reisandylamdjani.github.io/Lab5_Starter/explore.html)
+
+1. We would use unit testing to test the "message" feature as it is something that a user can do. We want the "message" to be able to write and send a message to another user. The receiving end of the text is in another test
+
+2. We would use unit testing to test the "max message length" because it is a hard coded number that can be tested. We can test to see the result of a message with more than 80 and less than 80 characters.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,122 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber
+
+// True tests
+test('verify that each component in the phone number has 3 digts, 3 digits, then 4 digits', () => {
+  let phoneNumber = "222-222-2222";
+  expect(isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+test('allowing parenthesis in the area code section', () => {
+  let phoneNumber = "(626)-222-2222";
+  expect(isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+// False Tests
+test('cannot accept characters in the phone number', () => {
+  let phoneNumber = "aaa-aaa-aaaa";
+  expect(isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+test('having no hypens', () => {
+  let phoneNumber = "2222222";
+  expect(isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+
+// isEmail
+
+// True tests
+test('allow any characters before the @ symbol', () => {
+  let email = "fsdkfasjkfasdf@gmail.com";
+  expect(isEmail(email)).toBe(true);
+});
+
+test('having 3 characters after the . is correct', () => {
+  let email = "fsdkfasjkfasdf@gmail.com";
+  expect(isEmail(email)).toBe(true);
+});
+
+// False tests
+test('doesnt allow other symbol', () => {
+  let email = "123!gmail.com";
+  expect(isEmail(email)).toBe(false);
+});
+
+test('would fail if there is not the @ symbol', () => {
+  let email = "123gmail.com";
+  expect(isEmail(email)).toBe(false);
+});
+
+// isStrongPassword
+
+test('first character is letter, contains letters, numbers and underscore with length under 15', () => {
+  let pass = "A8__29Bsz"
+  expect(isStrongPassword(pass)).toBe(true);
+});
+
+test('first character is letter, contains numbers, all upper case letters', () => {
+  let pass = "A9999BDH"
+  expect(isStrongPassword(pass)).toBe(true);
+});
+
+// FALSE TESTS
+test('length is under 4 characters', () => {
+  let pass = "AB"
+  expect(isStrongPassword(pass)).toBe(false);
+});
+
+test('first letter is not a letter', () => {
+  let pass = "90909BHDCD"
+  expect(isStrongPassword(pass)).toBe(false);
+});
+
+// isDate
+
+test('date with two digit month', () => {
+  let date = "01/12/2021"
+  expect(isDate(date)).toBe(true);
+});
+
+test('date with one digit month', () => {
+  let date = "3/21/2009"
+  expect(isDate(date)).toBe(true);
+});
+
+
+// FALSE TESTS
+test('year is only 2 digits long', () => {
+  let date = "03 / 21 / 09"
+  expect(isDate(date)).toBe(false);
+});
+
+test('not in correct format', () => {
+  let date = "03-21-2009"
+  expect(isDate(date)).toBe(false);
+});
+
+// isHexColor
+
+test('six digit hex color', () => {
+  let hexColor = "#1a2b3c"
+  expect(isHexColor(hexColor)).toBe(true);
+});
+
+test('three digit hex color', () => {
+  let hexColor = "789"
+  expect(isHexColor(hexColor)).toBe(true);
+});
+
+// FALSE TESTS
+test('color digits not in range of alphabet of hex color', () => {
+  let hexColor = "#GHIJKL"
+  expect(isHexColor(hexColor)).toBe(false);
+});
+
+test('four digit hex color, which is wrong', () => {
+  let hexColor = "#1234"
+  expect(isHexColor(hexColor)).toBe(false);
+});
